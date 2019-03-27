ec88b52775cad5bd7f5c31ab7d3c5ddf869e430b
Darren SooHoo <darsoohoo@gmail.com>
2019-03-26 20:36:51 -0700

    Created the genesis block

--- dev/test.js
+++ dev/test.js
@@ -2,27 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'OISDVSDVSD45645436346456';
-const currentBlockData = [
-    {
-        amount: 101,
-        sender: 'NGH68DFGDFGDFGFD',
-        recipient: 'FHJ6756GHDFHDDFGD'
-    },
-    {
-        amount: 30,
-        sender: '0987NGH68DFGDFGDFGFD',
-        recipient: 'UIGFHJ6756GHDFHDDFGD'
-    },
-    {
-        amount: 200,
-        sender: '456NGH68DFGDFGDFGFD',
-        recipient: 'AUSFHJ6756GHDFHDDFGD'
-    }
-];
-
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 54272));
-
-
-
+console.log(bitcoin);
